551665b6a4dc57582b52147f9bbe623f58b5a87f
morasternn <mstern@udesa.edu.ar>
2025-11-03 16:17:58 -0300

    Instalacion de firebase y expo

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+import app from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDLGd_008eMZ8yKO4EU1GSobHSMLXoRKog",
+    authDomain: "grupo4part2.firebaseapp.com",
+    projectId: "grupo4part2",
+    storageBucket: "grupo4part2.firebasestorage.app",
+    messagingSenderId: "125929398458",
+    appId: "1:125929398458:web:4f8c33d60310c4a4b62283"
+  };
+  
+app.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export const storage = app.storage()
+export const db = app.firestore()
